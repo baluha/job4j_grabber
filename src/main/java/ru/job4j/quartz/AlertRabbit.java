b3cdeb3789d4c163739a8928af0c90277c7959e1
baluha <romanbalushka@gmail.com>
2022-04-20 21:09:36 +0300

    0. Техническое задание. Агрегатор Java Вакансий.

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,51 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(new AlertRabbit().getProperties("rabbit.interval"))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException | IOException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public int getProperties(String key) throws  IOException {
+        String propValue;
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream("src/main/resources/log4j.properties")) {
+            properties.load(is);
+            propValue = properties.getProperty(key);
+            System.out.println(propValue);
+        }
+        return Integer.parseInt(propValue);
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
