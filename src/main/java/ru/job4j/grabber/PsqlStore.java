606b95e0682199b1a2c746e64c9384f3bcf2c7fc
baluha <romanbalushka@gmail.com>
2022-05-02 11:20:47 +0300

    5. PsqlStore

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -1,6 +1,9 @@
 package ru.job4j.grabber;
 
+import org.slf4j.Logger;
 import ru.job4j.quartz.AlertRabbit;
+
+import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 import java.time.LocalDateTime;
@@ -11,20 +14,21 @@ import java.sql.Timestamp;
 
 public class PsqlStore implements Store, AutoCloseable {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PsqlStore.class.getName());
     private Connection cn;
 
     public PsqlStore(Properties cfg) {
         try {
             Class.forName(cfg.getProperty("driver-class-name"));
         } catch (Exception e) {
-         throw new IllegalStateException(e);
+         LOG.error("Exception in reading properties file", e);
         }
         try {
             cn = DriverManager.getConnection(cfg.getProperty("url"),
                     cfg.getProperty("username"),
                     cfg.getProperty("password"));
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            LOG.error("Exception in reading properties file", e);
         }
     }
 
@@ -36,7 +40,7 @@ public class PsqlStore implements Store, AutoCloseable {
     }
 
     @Override
-    public void save(Post post) throws SQLException {
+    public void save(Post post)  {
         try (PreparedStatement ps = cn.prepareStatement(
                 "insert into post(title, description, link, created) values(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, post.getTitle());
@@ -49,11 +53,13 @@ public class PsqlStore implements Store, AutoCloseable {
                     post.setId(getGeneratedKeys.getInt(1));
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception in saving post in database", e);
         }
     }
 
     @Override
-    public List<Post> getAll() throws SQLException {
+    public List<Post> getAll(){
         List<Post> post = new ArrayList<>();
         try(PreparedStatement statement = cn.prepareStatement("Select * from post")) {
             try (ResultSet rslSet = statement.executeQuery()) {
@@ -61,28 +67,43 @@ public class PsqlStore implements Store, AutoCloseable {
                     post.add(getPost(rslSet));
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception in getting list of posts", e);
         }
         return post;
     }
 
     @Override
-    public Post findById(int id) throws SQLException {
-        Statement statement = cn.createStatement();
-        String sql = String.format("select * from post where id = %o", id);
-        ResultSet rsl = statement.executeQuery(sql);
-        return rsl.next() ? getPost(rsl) : null;
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement ps = cn.prepareStatement(
+                String.format("select * from post where id = %o", id))) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    post = getPost(resultSet);
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Exception in find by id method", e);
+        }
+        return post;
     }
 
-    public Post getPost(ResultSet rsl) throws SQLException {
-            return new Post(rsl.getInt("id"),
+    public Post getPost(ResultSet rsl){
+        Post post = new Post();
+        try {
+            post = new Post(rsl.getInt("id"),
                     rsl.getString("title"),
                     rsl.getString("link"),
                     rsl.getString("description"),
                     rsl.getTimestamp("created").toLocalDateTime());
-
+        } catch (Exception e) {
+            LOG.error("Exception in getPost method", e);
+        }
+        return post;
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         Properties properties = AlertRabbit.getProperties();
         PsqlStore psqlStore = new PsqlStore(properties);
         Post post = new Post(
@@ -91,8 +112,8 @@ public class PsqlStore implements Store, AutoCloseable {
                 "Сиди за монитором all day long и имитируй бурную деятельность",
                 LocalDateTime.now());
         psqlStore.save(post);
-        System.out.println(psqlStore.getAll());
+        System.out.println(psqlStore.findById(1));
         System.out.println("-|-|-|-|-|-|-|-|-|-|-");
-        System.out.println(psqlStore.findById(2));
+        psqlStore.getAll().forEach(System.out::println);
     }
 }
--- src/main/java/ru/job4j/grabber/TestClass.java
+++ src/main/java/ru/job4j/grabber/TestClass.java
@@ -3,10 +3,9 @@ package ru.job4j.grabber;
 import ru.job4j.grabber.utils.HarbCareerDateTimeParser;
 
 import java.io.IOException;
+import java.util.Properties;
 
 public class TestClass {
     public static void main(String[] args) throws IOException {
-        HabrCareerParse parse = new HabrCareerParse(new HarbCareerDateTimeParser());
-        parse.list("https://career.habr.com").stream().forEach(s -> System.out.println(s));
     }
 }
