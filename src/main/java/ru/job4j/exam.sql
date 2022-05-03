9394de6dfb1d205ee494622fd24604683cf2b2aa
baluha <romanbalushka@gmail.com>
2022-05-03 13:48:23 +0300

    2. SQL Select by company

--- src/main/java/ru/job4j/exam.sql
+++ src/main/java/ru/job4j/exam.sql
@@ -2,6 +2,7 @@ insert into company(id, name) values(1, 'samsung');
 insert into company(id, name) values(2, 'apple');
 insert into company(id, name) values(3, 'huawei');
 insert into company(id, name) values(4, 'lenovo');
+insert into company(id, name) values(5, 'motorola');
 
 insert into person(id, name, company_id) values(1, 'petrov', 1);
 insert into person(id, name, company_id) values(2, 'sidorov', 1);
@@ -13,20 +14,29 @@ insert into person(id, name, company_id) values(7, 'vasivkov', 4);
 insert into person(id, name, company_id) values(8, 'razlomov', 4);
 insert into person(id, name, company_id) values(9, 'pupkin', 4);
 insert into person(id, name, company_id) values(10, 'zubov', 2);
+insert into person(id, name, company_id) values(11, 'vizov', 5);
+insert into person(id, name, company_id) values(12, 'obrubov', 5);
 
-select person.name as name
-from person
-where company_id !=4;
+select p.name as person_name, c.name as company_name
+from person as p
+join company as c
+on p.company_id = c.id
+where c.id !=5;
 
 select p.name as Человек, c.name as Компания
 from person as p
 join company as c
 on p.company_id = c.id;
 
-SELECT c.name, COUNT(p.company_id)
-from company as c
-join person as p
+select c.name, count(p)
+from company c
+join person p
 on p.company_id = c.id
 group by c.name
-order by count desc
-limit 1;
+having count(p) =
+(select max(count) from
+(select c.name, count(p) from company c
+join person p
+on p.company_id = c.id
+group by c.name
+) as foo)
