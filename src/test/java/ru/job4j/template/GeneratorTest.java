b33a9f176f0637fec3d59a1cde2f302ed1162e6f
baluha <romanbalushka@gmail.com>
2022-06-12 02:40:19 +0300

    3. Шаблонизатор.

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.template;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneratorTest  {
+
+    @Test
+    public void whenGenerate() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Name", "Ivan");
+        map.put("Surname", "Ivanov");
+        map.put("Middle name", "Ivanovich");
+        Generator generator = new GeneratorFullName();
+        assertThat("Ivanov Ivan Ivanovich",
+                is(generator.produce("${Surname} ${name} ${Middle name}", map)));
+    }
+
+    @Test
+    public void whenExcessSample() throws IllegalArgumentException {
+        Map<String, String> map = new HashMap<>();
+        map.put("Name", "Ivan");
+        map.put("Middle name", "Ivanovich");
+        Generator generator = new GeneratorFullName();
+        assertThat("Ivanov Ivan Ivanovich",
+                is(generator.produce("${Surname} ${name} ${Middle name}", map)));
+    }
+
+    @Test
+    public void key() throws IllegalArgumentException {
+        Map<String, String> map = new HashMap<>();
+        map.put("Name", "Ivan");
+        map.put("Surname", "Ivanov");
+        map.put("Middle name", "Ivanovich");
+        Generator generator = new GeneratorFullName();
+        assertThat("Ivanov Ivan Ivanovich",
+                is(generator.produce("${name} ${Middle name}", map)));
+    }
+}
