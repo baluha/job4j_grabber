a0440a8bebb9e9fcdc1651ae35f8bbd42d96ff5e
baluha <romanbalushka@gmail.com>
2022-06-19 14:32:04 +0300

    2. Что такое TDD?

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -3,14 +3,17 @@ package ru.job4j.tdd;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void whenBuy() {
         Account account = new AccountCinema();
@@ -21,6 +24,7 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
     @Test
     public void whenFind() {
         Cinema cinema = new Cinema3D();
@@ -36,5 +40,31 @@ public class CinemaTest {
         cinema.add(session);
         List<Session> sessions = ((Cinema3D) cinema).getLst();
         assertThat(sessions.get(0), is(session));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPlaceAlreadyBuy() throws IllegalArgumentException {
+        Cinema cinema = new Cinema3D();
+        Account account1 = new AccountCinema();
+        Account account2 = new AccountCinema();
+        cinema.buy(account1, 1, 1, new GregorianCalendar(2022, Calendar.DECEMBER, 1));
+        cinema.buy(account2, 1, 1, new GregorianCalendar(2022, Calendar.DECEMBER, 1));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDateNotExist() throws IllegalArgumentException {
+        Cinema cinema = new Cinema3D();
+        Account account = new AccountCinema();
+        cinema.buy(account, 1, 1, new GregorianCalendar(1990, Calendar.DECEMBER, 1));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPlaceNotExist() throws IllegalArgumentException {
+        Cinema cinema = new Cinema3D();
+        Account account = new AccountCinema();
+        cinema.buy(account, 99, -1, new GregorianCalendar(2022, Calendar.DECEMBER, 1));
     }
 }
--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import junit.framework.TestCase;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 public class GeneratorTest  {
 
+    @Ignore
     @Test
     public void whenGenerate() {
         Map<String, String> map = new HashMap<>();
@@ -22,6 +24,7 @@ public class GeneratorTest  {
                 is(generator.produce("${Surname} ${name} ${Middle name}", map)));
     }
 
+    @Ignore
     @Test
     public void whenExcessSample() throws IllegalArgumentException {
         Map<String, String> map = new HashMap<>();
@@ -32,6 +35,7 @@ public class GeneratorTest  {
                 is(generator.produce("${Surname} ${name} ${Middle name}", map)));
     }
 
+    @Ignore
     @Test
     public void key() throws IllegalArgumentException {
         Map<String, String> map = new HashMap<>();
