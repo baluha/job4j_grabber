704323643b8d33aa5dd34014ee4dfd179e771dc6
baluha <romanbalushka@gmail.com>
2022-07-02 17:03:39 +0300

    0. Принцип открытости закрытости

--- src/main/java/ru/job4j/ood/ocp/Color.java
+++ src/main/java/ru/job4j/ood/ocp/Color.java
@@ -22,6 +22,17 @@ class Product {
         this.size = size;
     }
 
+    @Override
+    public String toString() {
+        return "Product{"
+                + "name='" + name + '\''
+                + ", color=" + color
+                + ", size=" + size
+                + '}';
+    }
+}
+
+
     class ProductFilter {
         public Stream<Product> filterByColor(List<Product> products, Color color) {
             return products.stream().filter(p -> p.color == color);
@@ -37,6 +48,48 @@ class Product {
                     && p.size == size);
         }
     }
+
+    interface Specification<T> {
+        boolean isSatisfied(T item);
+    }
+
+    interface Filter<T> {
+        Stream<T> filter(List<T> items, Specification<T> specification);
+        }
+
+        class ColorSpecification implements Specification<Product> {
+
+        Color color;
+
+            public ColorSpecification(Color color) {
+                this.color = color;
+            }
+
+            @Override
+            public boolean isSatisfied(Product item) {
+                return item.color == color;
+            }
+        }
+
+        class BetterFilter implements Filter<Product> {
+
+            @Override
+            public Stream<Product> filter(List<Product> items, Specification<Product> specification) {
+                return items.stream().filter(specification::isSatisfied);
+            }
+        }
+
+class Test {
+    public static void main(String[] args) {
+        Product product1 = new Product("samsung", Color.GREEN, Size.SMALL);
+        Product product2 = new Product("Toyota", Color.BLUE, Size.LARGE);
+        List<Product> lst = List.of(product1, product2);
+        BetterFilter bf = new BetterFilter();
+        bf.filter(lst, new ColorSpecification(Color.GREEN)).forEach(System.out::println);
+    }
 }
+
+
+
 
 
